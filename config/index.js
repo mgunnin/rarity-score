57efc414b158c4b1de4d372d5c769fb197272c2a
Matt Gunnin <matt@esportsone.com>
2022-04-10 17:21:25 -0500

    Updated with OneBot

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 export const config = {
   env: "prod",
   LOCAL_API_URL: "localhost:3000",
-  API_URL: "odp-rarity.vercel.app",
-  COLLECTION_NAME: "OneDayPunks",
-  COLLECTION_TITLE: "One Day Punks",
-  COLLECTION_DESCRIPTION: '10k "One Day I\'ll Be A Punk"-punks.',
+  API_URL: "rarity-score-coral.vercel.app/",
+  COLLECTION_NAME: "OneBot",
+  COLLECTION_TITLE: "OneBot",
+  COLLECTION_DESCRIPTION:
+    "OneBot is your digital companion for the esports and gaming metaverse. OneBot Genesis collection features 6,000 unique robotic avatars, each constructed with futuristic materials, glowing neon lights, and an animated expression displaying their personality. Each OneBot has it's own custom utility that enhances the functionality and appearance within the Esports Metaverse.",
   COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
   STARTING_INDEX: 0,
 };
